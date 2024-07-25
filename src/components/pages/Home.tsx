bb4325072aa902e6014b81fbce50c158533f4887
Jhony Pereira <jhony-16@live.com>
2024-07-25 05:01:21 -0300

    Aplicação  final - modulo 22

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -4,7 +4,7 @@ import CreateNotes from "../CreateNote/CreateNote";
 const HomePage = () => {
 	return (
 		<Container className="pt-24 flex flex-col items-center">
-			<h2 className="text-base mb-4 text-slate-200">Organize sua rotina com Notes</h2>
+			<h2 className="text-base mb-4 text-slate-200">Criar nota</h2>
 
 			<div className="">
 				<CreateNotes />
