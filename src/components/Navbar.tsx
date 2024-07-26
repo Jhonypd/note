7a6be04271e7ab2347214acc5a909e9e6777a13e
Jhony <jhony-16@live.com>
2024-07-26 10:27:58 -0300

    ajustado o menu, para fechar ao clicar em um item

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import { Navbar } from "react-bootstrap";
@@ -5,17 +6,38 @@ import Logo from "./Logo";
 import { Link } from "react-router-dom";
 
 const NavbarNotes = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => setExpanded(!expanded);
+  const handleClose = () => setExpanded(false);
+
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-white shadow-sm rounded-[10px]">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      expanded={expanded}
+      onToggle={handleToggle}
+      className="bg-white shadow-sm rounded-[10px]"
+    >
       <Container fluid className="justify-between">
         <Logo />
         <Navbar.Toggle aria-controls="responsive-navbar-nav" className="text-sm p-1" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto flex justify-center items-center">
-            <Nav.Link as={Link} to="/" aria-label="Ir para o início da página">
+            <Nav.Link
+              as={Link}
+              to="/"
+              onClick={handleClose}
+              aria-label="Ir para o início da página"
+            >
               Inicio
             </Nav.Link>
-            <Nav.Link as={Link} to="/notas" aria-label="Ir para a seção de Notas">
+            <Nav.Link
+              as={Link}
+              to="/notas"
+              onClick={handleClose}
+              aria-label="Ir para a seção de Notas"
+            >
               Notas
             </Nav.Link>
           </Nav>
